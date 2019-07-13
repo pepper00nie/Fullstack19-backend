27dea2579ace45eabb6e9d7564c4e4dc6c742d68
Juho Saastamoinen <juhosaastamoinen@icloud.com>
2019-07-13 20:01:23 +0300

    Tehtävien 3.5 ja 3.6 palautus

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ let persons = [
   }
 ]
 
-app.get('/info', (req, res) => {
+app.use(bodyParser.json())
 
+app.get('/info', (req, res) => {
   const info = `
     <p>Phonebook has ${persons.length} contacts.</p>
     <p>${new Date()}</p>
   `
-
   res.send(info)
 })
 
@@ -42,6 +42,29 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const note = (persons.find(n => n.id === Number(req.params.id)))
   note ? res.json(note) : res.status(404).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const name = req.body.name
+  const number = req.body.number
+
+  if (!name || !number) {
+    return res.status(400).json({ error: "missing name or number in request" })
+  }
+
+  if (persons.some(n => n.name === name)) {
+    return res.status(409).json({ error: "name already exists in phonebook" })
+  }
+
+  const newId = Math.floor(Math.random() * Math.floor(1000000))
+  const newPerson = {
+    name: name,
+    number: number,
+    id: newId
+  }
+  persons = persons.concat(newPerson)
+  
+  res.json(newPerson)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
